6fcd159b8b2563eb471d79e8749d70ab0c6eb72f
Wei Yang <yangw@slac.stanford.edu>
2017-10-25 23:35:00 -0700

    bug fix

--- sync-fat.sh
+++ sync-fat.sh
@@ -7,7 +7,7 @@ latestsingimg="/data/yangw/images/centos6-cvmfs.atlas.cern.ch.img"
 cmt="x86_64-slc6-gcc49-opt"
 
 script=$(readlink -f $0)
-scrpitdir=$(dirname $SCRIPT)
+scriptdir=$(dirname $script)
 
 exec > /tmp/sync-atlas-uncvmfs.log 2>&1
 date
@@ -21,12 +21,12 @@ mkdir -p cvmfs/atlas-condb.cern.ch cvmfs/atlas-nightlies.cern.ch cvmfs/sft.cern.
 # Directories needed by Titon
 mkdir -p lustre autofs ccs
 # /ustlas has to exist in order to run "singularity exec -w" at BNL
-mkdir -p usatlas
+mkdir -p usatlas 
 
 echo ""
 echo "---------- Update cvmfs"
 echo ""
-#uncvmfs -vv -n8 $HOME/local/etc/uncvmfs/uncvmfs.conf atlas
+#uncvmfs -vv -n16 /data/yangw/git/atlas-fat-container/uncvmfs.conf atlas
 # yampl is needed by Event Service
 rsync -aO --no-o --no-g --delete -H --no-A --no-X -v \
     /cvmfs/atlas.cern.ch/repo/sw/local/x86_64-slc5-gcc43-opt/yampl \
@@ -109,6 +109,7 @@ singularity exec -w -B /data/yangw/uncvmfs/root/cvmfs:/mnt \
         --filter='. /tmp/rsync-exclude.rules.txt' \
         --filter='P /atlas.cern.ch/repo/ATLASLocalRootBase' \
         --filter='+s /atlas.cern.ch/repo/sw/database/DBRelease' \
+        --filter='+s /atlas.cern.ch/repo/sw/database/GroupData' \
         --filter='-s /atlas.cern.ch/repo/sw/database/*' \
         --filter='-s /atlas.cern.ch/repo/sw/BOINC' \
         --filter='-s /atlas.cern.ch/repo/sw/Generators' \
@@ -127,7 +128,7 @@ tmpimg=$latestsingimg.TMP
 dd if=$latestsingimg bs=1M count=1 | dd ibs=31 skip=1 of=$tmpimg
 dd if=$latestsingimg bs=1M skip=1 of=$tmpimg oflag=append conv=notrunc
 sync
-resize2fs -f -M $tmpimg 
+/sbin/resize2fs -f -M $tmpimg 
 dd if=$latestsingimg ibs=31 count=1 > $latestsingimg.$cmt
 dd if=$tmpimg bs=1M of=$latestsingimg.$cmt oflag=append conv=notrunc
 rm $tmpimg
